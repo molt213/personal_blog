228a362789b8182fbc31c0ec2478a38d13c35a92
molt213 <molt213@users.noreply.github.com>
2026-09-02 12:18:15 +0800

    Publish Kancolle summer event record

--- content/posts/index.js
+++ content/posts/index.js
@@ -1,7 +1,18 @@
 import Mewtype from "./Mewtype.html";
+import kanc_26_9 from "./kanc_26_9.html";
 
 // 新文章按这个格式加到最前面，文章列表与网址会自动生成。
 export const POSTS = [
+  {
+    slug: "kanc_26_9",
+    category: "随记",
+    date: "2026.09.02",
+    title: "舰队collection26年夏活丁个人记录贴",
+    excerpt: "记录消耗以及出货情况",
+    lead: "记录消耗以及出货情况",
+    artLabel: "舰c26年\n夏活丁",
+    content: kanc_26_9
+  },
   {
     slug: "Mewtype",
     category: "随记",
